8d677ea8aa3e648467ea7e3ee31295da9b2752f0
Nikolas <niksan540@gmail.com>
2023-02-08 13:38:05 +0300

    for

---
+++ for/for.js
@@ -0,0 +1,16 @@
+// BEGIN (write your solution here)
+const encrypt = (text) => {
+  let res = '';
+  for (let i = 0; i < text.length; i += 1) {
+    if (i % 2 === 0 && i !== text.length - 1) {
+      res += text[i + 1];
+      res += text[i];
+    } else if (i % 2 === 0 && i === text.length - 1) {
+      res += text[i];
+    }
+  }
+  return res;
+};
+// END
+
+export default encrypt;
