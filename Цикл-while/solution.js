12973acca299d1629ee509c3bdcc34addf63e64d
Nikolas <niksan540@gmail.com>
2023-02-15 09:28:52 +0300

    123

---
+++ Цикл-while/solution.js
@@ -0,0 +1,13 @@
+// BEGIN
+const printNumbers = (initialNumber) => {
+  let i = initialNumber;
+  while (i > 0) {
+    console.log(i);
+    i -= 1;
+  }
+  console.log('finished!');
+};
+  // END
+
+export default printNumbers;
+
