1175f81e951ff8b4a8e0d0a5fe50011d8e5e7d98
Nikolas <niksan540@gmail.com>
2023-02-17 09:40:27 +0300

    тернарный-оператор

---
+++ Тернарный-оператор/convertText.js
@@ -0,0 +1,15 @@
+import reverse from './reverse';
+
+// BEGIN
+const convertText = (text) => {
+  if (text === '') {
+    return '';
+  }
+
+  const reversable = text[0] !== text[0].toUpperCase();
+  return reversable ? reverse(text) : text;
+};
+// END
+
+export default convertText;
+
