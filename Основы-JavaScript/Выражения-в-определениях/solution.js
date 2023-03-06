e8a340c167a337b2736f68529147b3db0dcfad35
Nikolas <niksan540@gmail.com>
2023-03-06 09:29:42 +0300

    06.03.2023

---
+++ Основы-JavaScript/Выражения-в-определениях/solution.js
@@ -0,0 +1,13 @@
+// @ts-check
+/* eslint-disable no-console */
+
+const eurosCount = 100;
+
+// BEGIN (write your solution here)
+const dollarCount = eurosCount * 1.25;
+console.log(dollarCount);
+const rubCount = dollarCount * 60;
+console.log(rubCount);
+
+// END
+
