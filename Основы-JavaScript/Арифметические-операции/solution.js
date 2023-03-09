4ec307de3016838404c79a54bea1e38db1380879
Nikolas <niksan540@gmail.com>
2023-03-09 14:52:51 +0300

    09.03.2023

---
+++ Основы-JavaScript/Арифметические-операции/solution.js
@@ -0,0 +1,10 @@
+// @ts-check
+/* eslint-disable no-console */
+
+// BEGIN (write your solution here)
+console.log(3 ** 5);
+console.log(-8 / -4);
+console.log(100 % 3);
+console.log((3 ** 5) + (-8 / -4) + (100 % 3));
+// END
+
