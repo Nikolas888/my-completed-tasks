6406dbcd761678da849865cb560b250a639d9ff4
Nikolas <niksan540@gmail.com>
2023-03-15 10:50:42 +0300

    15.03.2023

---
+++ JS:Массивы/Цикл-for/arrays.js
@@ -0,0 +1,11 @@
+// BEGIN (write your solution here)
+export default (coll, prefix) => {
+  const result = [];
+
+  for (let i = 0; i < coll.length; i += 1) {
+    result[i] = `${prefix} ${coll[i]}`;
+  }
+
+  return result;
+};
+// END
