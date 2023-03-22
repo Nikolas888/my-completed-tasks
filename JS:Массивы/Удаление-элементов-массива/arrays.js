74146510b5027b844b4855632f098f3956f96ede
Nikolas <niksan540@gmail.com>
2023-03-22 09:37:00 +0300

    22.03.2023

---
+++ JS:Массивы/Удаление-элементов-массива/arrays.js
@@ -0,0 +1,24 @@
+// @ts-check
+/* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
+
+// BEGIN (write your solution here)
+const getSameParity = (coll) => {
+  if (coll.length === 0) {
+    return [];
+  }
+
+  const result = [];
+  const remainder = Math.abs(coll[0] % 2);
+
+  for (const item of coll) {
+    if (Math.abs(item % 2) === remainder) {
+      result.push(item);
+    }
+  }
+
+  return result;
+};
+
+export default getSameParity;
+// END
+
