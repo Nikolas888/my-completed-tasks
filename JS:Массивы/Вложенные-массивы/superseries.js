e011a1c7876a0dcd369a66dba9478132b24b3592
Nikolas <niksan540@gmail.com>
2023-03-27 11:51:19 +0300

    27.03.2023

---
+++ JS:Массивы/Вложенные-массивы/superseries.js
@@ -0,0 +1,21 @@
+// @ts-check
+/* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
+
+// BEGIN (write your solution here)
+export default (scores) => {
+  let result = 0;
+  for (const score of scores) {
+    result += Math.sign(score[0] - score[1]);
+  }
+
+  if (result > 0) {
+    return 'canada';
+  }
+  if (result < 0) {
+    return 'ussr';
+  }
+
+  return null;
+};
+// END
+
