9260b5b6e90747192cf72bd15d8cb4225b9eacaf
Nikolas <niksan540@gmail.com>
2023-04-07 09:44:02 +0300

    07.04.2023

---
+++ JS:Массивы/Теория-Множеств/strings.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-restricted-syntax */
+
+// BEGIN (write your solution here)
+const countUniqChars = (text) => {
+  const uniqChars = [];
+
+  for (const char of text) {
+    if (!uniqChars.includes(char)) {
+      uniqChars.push(char);
+    }
+  }
+
+  return uniqChars.length;
+};
+
+export default countUniqChars;
+// END
+
